50a2195e9ba59720d36b97b7c713da1ac600957e
stephaneBalelos <stephanedondyas2@gmail.com>
2025-08-25 09:58:03 +0200

    Markers

--- page-garagenhofe.php
+++ page-garagenhofe.php
@@ -8,13 +8,39 @@ get_header('no-nav');
             <div class="easyrealestate-app-map">
                 <div class="map" style="background-image: url(<?php echo EASYREALESTATE_URL . '/dist/map.png'; ?>);">
                 </div>
+                <div class="easyrealestate-app-map-infobox">
+                    <div class="easyrealestate-app-map-infobox-content">
+                        <button close-btn class="easyrealestate-app-map-infobox-close-btn">&times;</button>
+                        <h2 title>Information</h2>
+                        <p content>Hier könnten Informationen zur ausgewählten Garage angezeigt werden.</p>
+                    </div>
+                </div>
             </div>
             <div class="easyrealestate-app-list">
                 <h1><?php the_title(); ?></h1>
                 <p><?php the_content(); ?></p>
                 <div class="easyrealestate-app-list-items-container">
                     <div class="easyrealestate-app-list-items">
-                        <div class="easyrealestate-app-list-item">
+                        <div class="easyrealestate-app-list-item"
+                            data-id="1"
+                            data-title="Garagenhof Beispiel 1"
+                            data-lat="53.53238971683533"
+                            data-lng="8.106689978965917"
+                        >
+                            <div class="easyrealestate-app-list-item-image">
+                                <img src="<?php echo EASYREALESTATE_URL . '/dist/garage-placeholder.jpg'; ?>" alt="<?php the_title(); ?>" />
+                            </div>
+                            <div class="easyrealestate-app-list-item-details">
+                                <h2>Garagenhof Beispiel 1</h2>
+                                <p>Beschreibung der Garage. Größe, Lage, Preis etc.</p>
+                            </div>
+                        </div>
+                        <div class="easyrealestate-app-list-item"
+                            data-id="2"
+                            data-title="Garagenhof Beispiel 2"
+                            data-lat="53.52225993419428"
+                            data-lng="8.122976342050313"
+                        >
                             <div class="easyrealestate-app-list-item-image">
                                 <img src="<?php echo EASYREALESTATE_URL . '/dist/garage-placeholder.jpg'; ?>" alt="<?php the_title(); ?>" />
                             </div>
--- src/components/easyrealestate-app.ts
+++ src/components/easyrealestate-app.ts
@@ -1,26 +1,36 @@
 import '../styles/_easyrealestate-app.scss';
 import { Loader } from "@googlemaps/js-api-loader"
 
+let map: google.maps.Map;
+let markers: Map<string, google.maps.marker.AdvancedMarkerElement> = new Map();
+let infobox: HTMLElement | null = null;
 
-export default function initEasyRealEstateApp() {
+function initEasyRealEstateApp() {
     const toggleButton = document.querySelector('.easyrealestate-app-view-toggle-button');
     const appContent = document.querySelector('.easyrealestate-app-content');
     
     if (!toggleButton || !appContent) {
         return;
     }
-
+    
     const mapContainer = appContent.querySelector('.easyrealestate-app-map');
     if (!mapContainer) {
         return;
     }
-
+    
     const mapElement = mapContainer.querySelector('.map');
     if (!mapElement) {
         return;
     }
-
+    
     initGoogleMaps(mapElement);
+
+    infobox = appContent.querySelector('.easyrealestate-app-map-infobox');
+    if (!infobox) {
+        return;
+    }
+
+    initInfoBox();
 
     toggleButton.addEventListener('click', () => {
         console.log('Toggle button clicked');
@@ -29,7 +39,7 @@ export default function initEasyRealEstateApp() {
 
 }
 
-function initGoogleMaps(mapElement: Element) {
+async function initGoogleMaps(mapElement: Element) {
     // 53.53238971683533, 8.106689978965917
 
     const mapOptions = {
@@ -37,7 +47,8 @@ function initGoogleMaps(mapElement: Element) {
         zoom: 14,
         zoomControl: false,
         cameraControl: false,
-        fullscreenControl: false
+        fullscreenControl: false,
+        mapId: "8c1e6a9bed70aa6c8b8bbedb"
     };
 
     const loader = new Loader({
@@ -45,15 +56,106 @@ function initGoogleMaps(mapElement: Element) {
         version: "weekly",
     });
 
-    loader
-        .importLibrary('maps')
-        .then(({ Map }) => {
-            new Map(mapElement, mapOptions);
-        })
-        .catch((e) => {
-            // do something
-            console.error(e);
-        });
+    try {
+        const { Map } = await loader.importLibrary('maps');
+        map = new Map(mapElement as HTMLElement, mapOptions);
+    } catch (e) {
+        console.error(e);
+    }
+
+    try {
+        const { AdvancedMarkerElement } = await loader.importLibrary('marker');
+        loadMarkers(AdvancedMarkerElement);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function initInfoBox() {
+    if (!infobox) {
+        return;
+    }
+    const closeButton = infobox.querySelector('[close-btn]');
+    if (closeButton) {
+        closeButton.addEventListener('click', hideInfoBox);
+    }
+}
+
+function loadMarkers(markerClass: typeof google.maps.marker.AdvancedMarkerElement) {
+    const items = document.querySelectorAll('.easyrealestate-app-list-item');
+    items.forEach(item => {
+        let latString = item.getAttribute('data-lat');
+        let lngString = item.getAttribute('data-lng');
+        if (!latString || !lngString) {
+            return;
+
+        }
+        const lat = parseFloat(latString);
+        const lng = parseFloat(lngString);
+        if (!isNaN(lat) && !isNaN(lng)) {
+            const markerOptions = {
+                position: { lat, lng },
+                map: map,
+                content: getMarkerLayout(item.getAttribute('data-title') || 'item ', item.getAttribute('data-content') || ''),
+            };
+            const newMarker = new markerClass(markerOptions);
+            newMarker.addListener('click', () => {
+                showInfoBox(item.getAttribute('data-id') || '', item.getAttribute('data-title') || '', item.getAttribute('data-content') || '');
+                map.panTo({ lat, lng });
+            });
+            markers.set(item.getAttribute('data-id') || '', newMarker);
+        }
+    });
+}
+
+
+
+function getMarkerLayout(title: string, content: string) {
+    const layout = document.createElement('div');
+    layout.classList.add('easyrealestate-app-marker-layout');
+    layout.innerHTML = `
+        <div class="easyrealestate-app-marker-content">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE --><path fill="currentColor" d="M22 9v11h-2v-9H4v9H2V9l10-4zm-3 3H5v2h14zm0 6H5v2h14zm0-3H5v2h14z"/></svg>
+        </div>
+    `;
+    return layout;
+}
+
+function showInfoBox(id: string, title: string, content: string) {
+    if (!infobox) {
+        return;
+    }
+    const titleEl = infobox.querySelector('[title]');
+    if (titleEl) {
+        titleEl.textContent = title;
+    }
+
+    const contentEl = infobox.querySelector('[content]');
+    if (contentEl) {
+        contentEl.innerHTML = content;
+    }
+
+    if(!infobox.classList.contains('easyrealestate-app-map-infobox-visible')) {
+        infobox.classList.add('easyrealestate-app-map-infobox-visible');
+    }
+
+    for (let [markerId, marker] of markers) {
+        const content = marker.content as HTMLElement;
+        console.log(markerId, id);
+        if (content.classList.contains('easyrealestate-app-marker-layout-active')) {
+            content.classList.remove('easyrealestate-app-marker-layout-active');
+        }
+        if (markerId === id) {
+            content.classList.add('easyrealestate-app-marker-layout-active');
+        }
+    }
+
+}
+
+function hideInfoBox() {
+    if (infobox) {
+        infobox.classList.remove('easyrealestate-app-map-infobox-visible');
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
